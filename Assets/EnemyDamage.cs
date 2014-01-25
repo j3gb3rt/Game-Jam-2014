b37fdcd4a80f8d361cb107833967022fcd046fd6
kayakninjas <chris@sinquefield.com>
2014-01-25 05:42:59 -0500

    Damage initial script prototype
    Didn't get much done here, but it kills the object. Attach it to things
    you want dead and it'll make it happen. I'll make it shiny later and
    introduce a system for different health values.
    K to damage self.

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamage : MonoBehaviour {
+	int health;
+
+	// Use this for initialization
+	void Start () {
+		health = 3;
+	}
+
+	void takeDamage() {
+		health -= 1;
+		transform.Rotate(new Vector3(0,30,0));
+	}
+
+	void takeDamage(int d) {
+		health -= d;
+	}
+
+	void Update() {
+		//Debug Code to test damage. Delete before shipping.
+		if (Input.GetKeyDown ("k")) {
+				takeDamage ();
+		} //end of debug code
+
+		if (health <= 0) {
+			Transform.Destroy (gameObject);
+		}
+	}
+}
