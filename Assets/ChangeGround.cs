a3ec41cf2d2842999506b5a67180a694ef28d567
kayakninjas <chris@sinquefield.com>
2014-01-25 00:05:32 -0500

    Scripts
    Compiler isn't working for some reason. Compile?

--- Assets/ChangeGround.cs
+++ Assets/ChangeGround.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Textures;
 
 public class ChangeGround : MonoBehaviour {
 
@@ -7,9 +8,20 @@ public class ChangeGround : MonoBehaviour {
 	void Start () {
 	
 	}
-	
+
+	void worldChange(int world) {
+			
+		}
+
 	// Update is called once per frame
 	void Update () {
-	
-	}
+		if(Input.GetKeyDown('1')) {
+			this.renderer.material = Fire_Color;
+			}
+		else if(Input.GetKeyDown('2')) {
+			this.renderer.material = Water_Color;
+		}
+		else if(Input.GetKeyDown('2')) {
+			this.renderer.material = Nature_Color;
+		}
 }
