a93c37791acb64a403e4cceeaa03e9ff6f77a86e
Zairo <patrickmcnealy@gmail.com>
2014-01-25 00:07:41 -0500

    SUMMRARRY
    COMMIT MESSAGER

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check for inputs
+
+		if (Input.GetKey ("a")){
+			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x - 1, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+		}
+		if (Input.GetKey ("d")){
+			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x + 1, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+		}
+		if (Input.GetKeyDown ("space")){
+			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, 10, this.rigidbody.velocity.z);
+		}
+
+	}
+}
