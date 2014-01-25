627557c176dcf601a91b700b87ef43f0b84459fc
j3gb3rt <jegbert.eggy@gmail.com>
2014-01-25 11:36:51 -0500

    PLAYER PREFERB

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -4,17 +4,39 @@ using System.Collections;
 public class MovingPlatform : MonoBehaviour {
 
 	private Vector3 initPosition;
-	public float X_Direction;
-	public float Y_Direction;
-	public float X_Distance;
-	public float Y_Distance;
+	private int firstMovement;
+	public float xVelocity;
+	public float yVelocity;
+	public float xDistance;
+	public float yDistance;
+	private float xMin;
+	private float xMax;
+	private float yMin;
+	private float yMax;
+	
 	// Use this for initialization
 	void Start () {
 		initPosition = transform.position;
+		firstMovement = true;
+		if (initPosition.x < initPosition.x + xDistance) {
+			xMin = initPosition.x;
+			xMax = initPosition.x + xDistance;
+		} else {
+			xMin = initPosition.x + xDistance;
+			xMax = initPosition.x;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		transform.position.x += xDistance;
 
+
+		if (transform.position.x == initPosition.x) {
+
+		}
+		else if (transform.position.x == (initPosition.x + xVelocity)) {
+
+		}
 	}
 }
