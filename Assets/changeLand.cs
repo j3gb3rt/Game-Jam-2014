7a921ef0a33b2321a7c127fe1075f8920ce48ea1
kayakninjas <chris@sinquefield.com>
2014-01-25 01:55:30 -0500

    Moved Textures
    Moved Textures folder
    Deleted obsolete ChangeGround

---
+++ Assets/changeLand.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeLand : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("1")) {
+			this.renderer.material.color = Color.blue;
+		} else if (Input.GetKey ("2")) {
+			this.renderer.material.color = Color.red;
+		} else if (Input.GetKey ("3")) {
+			this.renderer.material.color = Color.green;
+		}
+
+	}
+}
