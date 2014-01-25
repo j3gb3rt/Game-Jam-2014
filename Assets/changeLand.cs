30a8c82864b15e9e631d63e8dc5a890610e9e209
kayakninjas <chris@sinquefield.com>
2014-01-25 04:50:36 -0500

    Texture Update
    Got the texture change for real this time.
    Also includes the first part of the changeWater method.

--- Assets/changeLand.cs
+++ Assets/changeLand.cs
@@ -2,22 +2,22 @@
 using System.Collections;
 
 public class changeLand : MonoBehaviour {
-	private Material NatureMaterial, IceMaterial, FireMaterial;
+	private Material NatureMaterial, FrostMaterial, FireMaterial;
 
 	void Start () {
 		FireMaterial = Resources.Load<Material>("Fire_Color");
-		IceMaterial = Resources.Load<Material>("Ice_Color");
+		FrostMaterial = Resources.Load<Material>("Frost_Color");
 		NatureMaterial = Resources.Load<Material>("Nature_Color");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey ("1")) {
-			this.renderer.material.color = Color.blue;
+			this.renderer.material = FrostMaterial;
 		} else if (Input.GetKey ("2")) {
-			this.renderer.material.color = Color.red;
+			this.renderer.material = FireMaterial;
 		} else if (Input.GetKey ("3")) {
-			this.renderer.material.color = Color.green;
+			this.renderer.material = NatureMaterial;
 		}
 
 	}
---
+++ Assets/changeWater.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeWater : MonoBehaviour {
+	private Material WaterMaterial, IceMaterial, LavaMaterial;
+	
+	void Start () {
+		LavaMaterial = Resources.Load<Material>("Lava_Color");
+		IceMaterial = Resources.Load<Material>("Ice_Color");
+		WaterMaterial = Resources.Load<Material>("Water_Color");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("1")) {
+			this.renderer.material = IceMaterial;
+			this.collider.isTrigger = false;
+		} else if (Input.GetKey ("2")) {
+			this.renderer.material = LavaMaterial;
+			this.collider.isTrigger = true;
+		} else if (Input.GetKey ("3")) {
+			this.renderer.material = WaterMaterial;
+			this.collider.isTrigger = true;
+		}
+		
+	}
+}
