982cc99519daca2e150cfc68e166d8c46935e44a
Zairo <patrickmcnealy@gmail.com>
2014-01-25 00:41:18 -0500

    replacing
    kk

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -12,14 +12,14 @@ public class PlayerInput : MonoBehaviour {
 	void Update () {
 		//check for inputs
 
-		if (Input.GetKey ("a")){
-			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x - 1, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+		if (Input.GetKey ("a") && this.rigidbody.velocity.x > -15){
+			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x - 1.5f, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
 		}
-		if (Input.GetKey ("d")){
-			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x + 1, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+		if (Input.GetKey ("d") && this.rigidbody.velocity.x < 15){
+			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x + 1.5f, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
 		}
-		if (Input.GetKeyDown ("space")){
-			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, 10, this.rigidbody.velocity.z);
+		if (Input.GetKeyDown ("space")){	// recommended gravity is Y=-40;
+			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, 20, this.rigidbody.velocity.z);
 		}
 
 	}
