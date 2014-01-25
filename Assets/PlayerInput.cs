4eba5b8df9f3c0caaf76fcc0512cd21e085ef589
Zairo <patrickmcnealy@gmail.com>
2014-01-25 01:56:19 -0500

    No double jumps!

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class PlayerInput : MonoBehaviour {
-
+	int jump;
 	// Use this for initialization
 	void Start () {
-		
+		jump = 0;
 	}
 	
 	// Update is called once per frame
@@ -18,9 +18,17 @@ public class PlayerInput : MonoBehaviour {
 		if (Input.GetKey ("d") && this.rigidbody.velocity.x < 15){
 			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x + 1.5f, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
 		}
-		if (Input.GetKeyDown ("space")){	// recommended gravity is Y=-40;
+		if (Input.GetKeyDown ("space") && jump == 1){	// recommended gravity is Y=-40;
 			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, 20, this.rigidbody.velocity.z);
 		}
 
+	}
+
+	void OnCollisionEnter(){
+		jump++;
+	}
+
+	void OnCollisionExit(){
+		jump--;
 	}
 }
