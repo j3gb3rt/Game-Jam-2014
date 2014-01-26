5c98f42066c1c58afa06c6cf1bf4232cf1df9d0d
kayakninjas <chris@sinquefield.com>
2014-01-25 21:47:44 -0500

    Fire and Explosions
    Made fireballs and explosions, need to debug fireballs exploding on
    player at launch.

---
+++ Assets/FireballBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter() {
+		GameObject exploder = Resources.Load<GameObject>("Explosion");
+		Instantiate(exploder, transform.position, transform.localRotation);
+		Transform.Destroy (gameObject);
+	}
+}
--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class PlayerInput : MonoBehaviour {
 	int jump;
+	public GameObject fire;
+	private float speed;
+	private int facing;
 
 	// Use this for initialization
 	void Start () {
 		jump = 0;
+		speed = 20f;
+		fire = Resources.Load<GameObject>("fireball");
+		facing = 1;
 	}
 	
 	// Update is called once per frame
@@ -15,18 +21,31 @@ public class PlayerInput : MonoBehaviour {
 
 		if (Input.GetKey ("a") && this.rigidbody.velocity.x > -15){
 			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x - 1.5f, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+			facing = 0;
 		}
 		if (Input.GetKey ("d") && this.rigidbody.velocity.x < 15){
 			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x + 1.5f, this.rigidbody.velocity.y, this.rigidbody.velocity.z);
+			facing = 1;
 		}
 		if (Input.GetKeyDown ("space") && jump > 0){	// recommended gravity is Y=-40;
 			this.rigidbody.velocity = new Vector3(this.rigidbody.velocity.x, 25, this.rigidbody.velocity.z);
 		}
 		if (Input.GetKeyDown ("j")){
-			//fireball fire = (fireball) Instantiate (fireball, transform.position, transform.rotation);
-			//Rigidbody go = (Rigidbody)Instantiate(Resources.Load("fireball.prefab.meta"));
+			shootFireball();
 		}
 
+	}
+
+	void shootFireball(){
+		if (facing == 0) {
+			GameObject thisBoom = (GameObject)Instantiate (fire, transform.position - new Vector3(1.5f,0f,0f), transform.localRotation);
+			thisBoom.transform.Rotate (0,90,0);
+			thisBoom.rigidbody.velocity = new Vector3(-speed, 0, 0);
+		} else {
+			GameObject thisBoom = (GameObject)Instantiate (fire, transform.position + new Vector3(1.5f,0f,0f), transform.localRotation);
+			thisBoom.transform.Rotate (0,-90,0);
+			thisBoom.rigidbody.velocity = new Vector3(speed, 0, 0);
+		}
 	}
 
 	void OnCollisionEnter(){
